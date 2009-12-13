2188bdd713b9979b8c6c833c83b145902875b146
ezelspinguin <ezelspinguin@716d754a-e7d5-11de-93f0-cd01e99e34ab>
2009-12-13 13:45:45 +0000

    start of controls

--- gen/com/google/code/androidspycam/R.java
+++ gen/com/google/code/androidspycam/R.java
@@ -19,6 +19,14 @@ public final class R {
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
+    public static final class id {
+        public static final int b_bottom=0x7f070005;
+        public static final int b_left=0x7f070002;
+        public static final int b_right=0x7f070003;
+        public static final int b_top=0x7f070004;
+        public static final int gestures=0x7f070000;
+        public static final int image=0x7f070001;
+    }
     public static final class layout {
         public static final int main=0x7f030000;
     }
@@ -26,13 +34,16 @@ public final class R {
         public static final int license_short=0x7f040000;
     }
     public static final class string {
+        public static final int about_backup=0x7f060006;
         public static final int about_comments=0x7f060002;
         public static final int about_copyright=0x7f060003;
         public static final int about_website_label=0x7f060004;
         public static final int about_website_url=0x7f060005;
         public static final int app_name=0x7f060001;
         public static final int hello=0x7f060000;
-        public static final int menu_about=0x7f060006;
-        public static final int menu_preferences=0x7f060007;
+        public static final int link_about_dialog=0x7f060007;
+        public static final int market_backup=0x7f060008;
+        public static final int menu_about=0x7f060009;
+        public static final int menu_preferences=0x7f06000a;
     }
 }
