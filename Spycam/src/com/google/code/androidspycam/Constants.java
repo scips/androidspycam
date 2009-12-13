e040e79cf70d4a409186be8ecbef149a46adff28
friedger <friedger@716d754a-e7d5-11de-93f0-cd01e99e34ab>
2009-12-13 18:02:00 +0000

    Constants for preferences

---
+++ Spycam/src/com/google/code/androidspycam/Constants.java
@@ -0,0 +1,11 @@
+package com.google.code.androidspycam;
+
+public class Constants {
+	String prefId = "pref_id";
+	String prefSpeed = "pref_speed";
+	String prefPortNo = "pref_port_no";
+	String prefInputMode = "pref_input_mode";
+	String prefRefreshRate = "pref_refresh_rate";
+	String prefAngleV = "pref_angle_v";
+	String prefAngleH = "pref_angle_h";
+}
